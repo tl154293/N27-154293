1e0a8de573da5a25c1152f674a0dc91abde80d1c
tl154293 <tl154293@students.berufskolleg-borken.de>
2022-03-31 09:09:30 +0200

    Profilseite erstellt

--- server.js
+++ server.js
@@ -1,141 +1,276 @@
-// Programme verarbeiten oft Objekte der realen Welt. Objekte haben 
+// Programme verarbeiten oft Objekte der realen Welt. Objekte haben
+
 // Eigenschaften. In unserem Bankingprogramm interessieren uns Objekte,
+
 // wie z.B. Kunde, Konto, Filiale, Bankautomat, ...
+
 // Alle Kunden unserer Bank haben dieselben Eigenschaften, aber
+
 // unterschiedliche Eigenschaftswerte.
 
+ 
+
 class Kunde{
+
     constructor(){
+
         this.IdKunde
+
         this.Nachname
+
         this.Vorname
+
         this.Kennwort
+
         this.Kontostand
+
         this.Geburtsdatum
+
         this.Mail
+
         this.Rufnummer
+
     }
+
 }
 
-// Von der Kunden-Klasse wird eine konkrete Instanz gebildet. 
+ 
+
+// Von der Kunden-Klasse wird eine konkrete Instanz gebildet.
+
+ 
 
 let kunde = new Kunde()
 
+ 
+
 // Die konkrete Instanz bekommt Eigenschaftswerte zugewiesen.
 
-kunde.IdKunde = 154293
-kunde.Nachname = "Peterson"
-kunde.Vorname = "HAns"
-kunde.Geburtsdatum = "22.10.2022"
-kunde.Mail = "Peterson.Hans@web.de"
-kunde.Kennwort = "123456"
+ 
+
+kunde.IdKunde = 150000
+
+kunde.Nachname = "Müller"
+
+kunde.Vorname = "Pit"
+
+kunde.Geburtsdatum = "23.10.2000"
+
+kunde.Mail = "mueller@web.de"
+
+kunde.Kennwort = "123"
+
 kunde.Rufnummer = "+49123/4567890"
 
+ 
+
 const express = require('express')
+
 const bodyParser = require('body-parser')
+
 const meineApp = express()
+
 const cookieParser = require('cookie-parser')
+
 meineApp.set('view engine', 'ejs')
+
 meineApp.use(express.static('public'))
+
 meineApp.use(bodyParser.urlencoded({extended: true}))
+
 meineApp.use(cookieParser('geheim'))
 
+ 
+
 const server = meineApp.listen(process.env.PORT || 3000, () => {
+
     console.log('Server lauscht auf Port %s', server.address().port)    
+
 })
+
+ 
 
 // Die Methode meineApp.get('/' ...) wird abgearbeitet, sobald
+
 // der Kunde die Indexseite (localhost:3000 bzw. n27.herokuapp.com) ansurft.
 
+ 
+
 meineApp.get('/',(browserAnfrage, serverAntwort, next) => {              
-    
+
+   
+
     // Wenn ein signierter Cookie mit Namen 'istAngemeldetAls' im Browser vorhanden ist,
-    // dann ist die Prüfung wahr und die Anweisungen im Rumpf der if-Kontrollstruktur 
+
+    // dann ist die Prüfung wahr und die Anweisungen im Rumpf der if-Kontrollstruktur
+
     // werden abgearbeitet.
 
+ 
+
     if(browserAnfrage.signedCookies['istAngemeldetAls']){
-        
+
+       
+
         // Die Index-Seite wird an den Browser gegeben:
 
+ 
+
         serverAntwort.render('index.ejs',{})
+
     }else{
+
+ 
 
         // Wenn der Kunde noch nicht eigeloggt ist, soll
+
         // die Loginseite an den Browser zurückgegeben werden.
+
         serverAntwort.render('login.ejs', {
-            Meldung : ""
+
+            meldung : ""
+
         })
-    }                 
+
+    }                
+
 })
+
+ 
 
 // Die Methode meineApp.post('/login' ...) wird abgearbeitet, sobald
+
 // der Anwender im Login-Formular auf "Einloggen" klickt.
 
+ 
+
 meineApp.post('/login',(browserAnfrage, serverAntwort, next) => {              
-    
+
+   
+
     // Die im Browser eingegebene IdKunde und Kennwort werden zugewiesen
+
     // an die Konstanten namens idKunde und kennwort.
 
+ 
+
     const idKunde = browserAnfrage.body.IdKunde
+
     const kennwort = browserAnfrage.body.Kennwort
-    
+
+   
+
     console.log("ID des Kunden: " + idKunde)
+
     console.log("Kennwort des Kunden: " + kennwort)
 
+ 
+
     // Die Identität des Kunden wird überprüft.
-    
+
+   
+
     if(idKunde == kunde.IdKunde && kennwort == kunde.Kennwort){
-    
+
+   
+
         // Ein Cookie namens 'istAngemeldetAls' wird beim Browser gesetzt.
+
         // Der Wert des Cookies ist das in eine Zeichenkette umgewandelte Kunden-Objekt.
+
         // Der Cookie wird signiert, also gegen Manpulationen geschützt.
 
+ 
+
         serverAntwort.cookie('istAngemeldetAls',JSON.stringify(kunde),{signed:true})
+
         console.log("Der Cookie wurde erfolgreich gesetzt.")
 
+ 
+
         // Wenn die Id des Kunden mit der Eingabe im Browser übereinstimmt
+
         // UND ("&&") das Kennwort ebenfalls übereinstimmt,
+
         // dann gibt der Server die gerenderte Index-Seite zurück.
-        
+
+       
+
         serverAntwort.render('index.ejs', {})
+
     }else{
 
+ 
+
         // Wenn entweder die eingegebene Id oder das Kennwort oder beides
+
         // nicht übereinstimmt, wird der Login verweigert. Es wird dann die
+
         // gerenderte Login-Seite an den Browser zurückgegeben.
 
+ 
+
         serverAntwort.render('login.ejs', {
+
             Meldung : "Ihre Zugangsdaten scheinen nicht zu stimmen."
+
         })
+
     }
+
 })
 
+ 
 
 // Wenn die login-Seite im Browser aufgerufen wird, ...
 
+ 
+
 meineApp.get('/login',(browserAnfrage, serverAntwort, next) => {              
 
+ 
+
     // ... dann wird die login.ejs vom Server gerendert an den
+
     // Browser zurückgegeben:
+
+ 
 
     // Der Cookie wird gelöscht.
 
+ 
+
     serverAntwort.clearCookie('istAngemeldetAls')
 
+ 
+
     serverAntwort.render('login.ejs', {
+
         Meldung: "Bitte geben Sie die Zugangsdaten ein."
+
     })          
+
 })
 
+ 
+
 // Die meineApp.post('login') wird ausgeführt, sobald der Button
+
 // auf dem Login-Formular gedrückt wird.
 
+ 
 
 meineApp.get('/about',(browserAnfrage, serverAntwort, next) => {              
 
+ 
+
     serverAntwort.render('about.ejs', {
+
     })          
+
 })
+
+ 
 
 meineApp.get('/profile',(browserAnfrage, serverAntwort, next) => {              
 
@@ -144,33 +279,75 @@ meineApp.get('/profile',(browserAnfrage, serverAntwort, next) => {
         Nachname: kunde.Nachname,
         Mail: kunde.Mail,
         Rufnummer: kunde.Rufnummer,
-        Kennwort: kunde.Kennwort
+        Kennwort: kunde.Kennwort,
+        Erfolgsmeldung: ""
     })          
 })
-
+ 
 // Sobald der Speichern-Button auf der Profile-Seite gedrückt wird,
 // wird die meineApp.post('profile'...) abgearbeitet.
 
-meineApp.post('/profile',(browserAnfrage, serverAntwort, next) => {              
-    
-    // Der Wert der Eigenschaft von Mail im Browser wird
-    // zugewiesen (=) an die Eigenschaft Mail des Objekts kunde
+meineApp.post('/profile',(browserAnfrage, serverAntwort, next) => {  
 
-    kunde.Mail = browserAnfrage.body.Mail
+    // die Erfolgsmeldung für das Speichern der geänderten
+    // Profildaten wird in eine lokale Variable namens
+    // erfolgsmeldung gespeichert.
+
+    let erfolgsmeldung = ""
+
+    // Der Wert der Eigenschaft von Mail im Browser wird
+    // zugewiesen (=) an die Eigenschaft Mail des Objekts kunde 
+
+    if(kunde.Mail !== browserAnfrage.body.Mail){
+
+        // Wenn der Wert der Eigenschaft von kunde abweicht
+        // vom Wert der Eigenschaft Mail aus dem Browser-Formular
+        // dann wird die Erfolgsmeldung initialisert
+
+        erfolgsmeldung = erfolgsmeldung + "Änderung der Mail erfolgreich. "
+        kunde.Mail = browserAnfrage.body.Mail
+        console.log(erfolgsmeldung)
+
+    }
+
+    if(kunde.Kennwort !== browserAnfrage.body.Kennwort){
+ 
+        // Wenn der Wert der Eigenschaft von kunde abweicht
+        // vom Wert der Eigenschaft Kennwort aus dem Browser-Formular
+        // dann wird die Erfolgsmeldung initialisert
+
+        erfolgsmeldung = erfolgsmeldung + "Änderung des Kennworts erfolgreich. "
+        kunde.Kennwort = browserAnfrage.body.Kennwort
+        console.log(erfolgsmeldung)
+    }
+
+    if(kunde.Rufnummer !== browserAnfrage.body.Rufnummer){
+
+        // Wenn der Wert der Eigenschaft von kunde abweicht
+        // vom Wert der Eigenschaft Rufnummer aus dem Browser-Formular
+        // dann wird die Erfolgsmeldung initialisert
+
+        erfolgsmeldung = erfolgsmeldung + "Änderung der Rufnummer erfolgreich. "
+        kunde.Rufnummer = browserAnfrage.body.Rufnummer
+        console.log(erfolgsmeldung)
+
+    }
+
     kunde.Kennwort = browserAnfrage.body.Kennwort
     kunde.Rufnummer = browserAnfrage.body.Rufnummer
-    
     console.log("Profil gespeichert.")
-    
+
     serverAntwort.render('profile.ejs', {
+
         Vorname: kunde.Vorname,
         Nachname: kunde.Nachname,
         Mail: kunde.Mail,
         Rufnummer: kunde.Rufnummer,
-        Kennwort: kunde.Kennwort
+        Kennwort: kunde.Kennwort,
+        Erfolgsmeldung: erfolgsmeldung
     })
-})
 
+})
 
 // require('./Uebungen/ifUndElse.js')
 // require('./Uebungen/klasseUndObjekt.js')
